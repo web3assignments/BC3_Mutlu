7d44a86ce98a2c7a272b68d9aba52c2f8f06a15c
owner070 <39405408+owner070@users.noreply.github.com>
2020-11-17 19:29:16 +0100

    Added more functions

--- PD-02/Game.sol
+++ PD-02/Game.sol
@@ -1,17 +1,32 @@
-pragma solidity ^0.7.0;
+pragma solidity >=0.4.22 <0.8.0;
 
 contract Game {
     uint256 _stake;
+    address _currentAddress;
 
-    constructor(uint256 stake) public {
+    constructor(uint256 stake, address currentAddress) public {
         _stake = stake;
+        _currentAddress = currentAddress;
     }
 
-    function isItEven(uint256 stake) public returns (bool) {
-        bool isEven = false;
+    function guessIfEven(uint256 stake) public {
         if (stake % 2 == 0) {
-            return isEven = true;
+            calculateWinning(stake);
         }
-        return isEven;
+    }
+
+    function calculateWinning(uint256 stake) public {
+        uint256 percentage = (stake / _currentAddress.balance) * 100;
+        if (percentage > 50) {
+            sendRewardToAddress(stake * 3);
+        } else {
+            sendRewardToAddress(stake * 2);
+        }
+    }
+
+    function sendRewardToAddress(uint256 winning) public {
+        address payable sendAbleAddress = payable(_currentAddress);
+        (bool transferSucceeded, ) = sendAbleAddress.call{value: winning}("");
+        require(transferSucceeded, "Transfer failed, you lost everything.");
     }
 }
