b7475d130928f47c1b54461a8b217320eb2c5b1d
owner070 <39405408+owner070@users.noreply.github.com>
2020-12-11 16:01:57 +0100

    rinkeby deploy

--- PD-07/migrations/1_initial_migration.js
+++ PD-07/migrations/1_initial_migration.js
@@ -1,8 +1,8 @@
 var Game = artifacts.require("Game");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(Game, {value: 8000000000000000000});
+    await deployer.deploy(Game, {value: 1000000000000000000});
     gameContract = await Game.deployed();
-    await gameContract.GuessIfEven(2, {value: 2000000000000000000});
+    await gameContract.GuessIfEven(1, {value: 1000000000000000000});
     console.log(await gameContract.CalculatedWinnings())
 }
