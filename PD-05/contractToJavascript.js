e749bebb1a40c038d27b5ec0f5d33715fbec7c3f
owner070 <39405408+owner070@users.noreply.github.com>
2020-12-18 15:43:16 +0100

    .

--- PD-05/contractToJavascript.js
+++ PD-05/contractToJavascript.js
@@ -107,10 +107,9 @@ async function Contract() {
 	//contract instance
 	contract = new web3.eth.Contract(abi, contractAddress)
 	contract.methods.GuessIfEven(0).send({ from: account, value: Web3.utils.toWei('0', 'ether') }, function (error) {
-		if(error.message != null){
-		document.getElementById('outPut').innerHTML = error.message;
+		document.getElementById('outPut').innerHTML = error;
 		console.log("outPut Error: ", error);
-		}	
+		
 	}).then(function (receipt) {
 		document.getElementById('outPut').innerHTML = receipt.events.returnValues.winner;
 		document.getElementById('outPutAmount').innerHTML = receipt.events.returnValues.winning;
