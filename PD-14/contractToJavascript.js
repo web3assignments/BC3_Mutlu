0951748c5deb8bf80239a4e61296f74e5b984c19
owner070 <39405408+owner070@users.noreply.github.com>
2021-01-25 14:09:10 +0100

    Added ENS

---
+++ PD-14/contractToJavascript.js
@@ -0,0 +1,126 @@
+// contractAddress and abi are setted after contract deploy
+var contractAddress = '0xa491Ee40C399232Ba2cD109Ec975986FbCa16688';
+var abi = [
+	{
+		"inputs": [],
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "winner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "winning",
+				"type": "uint256"
+			}
+		],
+		"name": "AddressThatLost",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "test",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "winner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "winning",
+				"type": "uint256"
+			}
+		],
+		"name": "CalculatedWinnings",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "stake",
+				"type": "uint256"
+			}
+		],
+		"name": "GuessIfEven",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "currentAddressesInContract",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+async function Contract() {
+	// web3 provider with fallback for old version
+	web3 = await new Web3(Web3.givenProvider);
+	var requestAccounts = await web3.eth.requestAccounts();
+	console.log('Injected web3 detected.');
+
+	// } else {
+	// 	// Fallback to localhost if no web3 injection. We've configured this to GANACHE
+	// 	var provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545')
+	// 	web3 = new Web3(provider)
+	// 	console.log('No web3 instance injected, using Local web3.');
+	// }
+
+	// Accounts	
+	var account;
+	var accounts = await web3.eth.getAccounts();
+	account = accounts[0];
+	console.log('Account: ' + account);
+	document.getElementById('currentAddress').innerHTML = account;
+
+	
+	//contract instance
+	const name = 'gameguessifeven.eth';
+	var ensAddress = await web3.eth.ens.getAddress(name);
+
+	contract = new web3.eth.Contract(abi, ensAddress)
+	contract.methods.GuessIfEven(0).send({ from: account, value: Web3.utils.toWei('0', 'ether') }, function (error) {
+		document.getElementById('outPut').innerHTML = error;
+		console.log("outPut Error: ", error);
+		
+	}).then(function (receipt) {
+		console.log("Succes outPut Receipt: ", receipt)	
+		document.getElementById('outPut').innerHTML = receipt.events.CalculatedWinnings.returnValues.winner;
+		document.getElementById('outPutAmount').innerHTML = receipt.events.CalculatedWinnings.returnValues.winning;
+	});
+
+	web3.eth.getBalance(contractAddress, function (err, result) { console.log("Balance: ",result); });
+	console.log("Contract: ", contract);
+}
+Contract();
